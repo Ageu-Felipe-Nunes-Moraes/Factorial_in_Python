22ce58fa1cfb9bb8e3135fdef0e7c28a98431dd4
Ageu Moraes <ageumoraes67@gmail.com>
2024-04-14 12:14:30 -0300

    Padronizando nomenclatura de nomes variáveis

--- AlgoritimoFatorialEmPython/calculoFatorial.py
+++ AlgoritimoFatorialEmPython/calculoFatorial.py
@@ -8,38 +8,37 @@ class CalculaFatorial:
         self.fatorial = 0
         self.resultado = 1
     
-    def getValor(self):
+    def get_valor(self):
 
         self.fatorial = int(input("Digite um valor para saber o fatorial do mesmo: "))
         self.resultado = 1
 
-    def verificaCondicoes(self):
+    def verifica_condicoes(self):
         if self.fatorial < 0:
             print("Valor negativo, portanto resultado inválido.")
         else:
             for i in range(1, self.fatorial +1):
                 self.resultado *= i
-            self.imprimeResultado()
+            self.imprime_resultado()
             
 
-    def imprimeResultado(self):
+    def imprime_resultado(self):
         print(f"O fatorial do seu número é: {self.resultado}")
 
 calculadora = CalculaFatorial()
-calculadora.getValor()
-calculadora.verificaCondicoes()
+calculadora.get_valor()
+calculadora.verifica_condicoes()
 
 
 #Forma simplificada de calcular fatorial
-"""def calcula_fatorial(numero):
-    fatorial = 1
-    if numero >= 0:
-        for i in range(1, numero+1):
-            fatorial *= i
-        print(f"O fatorial é: {fatorial}")
-    else:
-        print("Valor inválido")
+#def calcula_fatorial(numero):
+#    fatorial = 1
+#    if numero >= 0:
+#        for i in range(1, numero+1):
+#            fatorial *= i
+#        print(f"O fatorial é: {fatorial}")
+#    else:
+#        print("Valor inválido")
 
-n = int(input("Digite um número: "))
-calcula_fatorial(n)"""
-
+#n = int(input("Digite um número: "))
+#calcula_fatorial(n)
