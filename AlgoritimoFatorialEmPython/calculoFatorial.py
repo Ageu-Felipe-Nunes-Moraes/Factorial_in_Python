100ec77a4cc97a812bc6cfbdaca8863bd4f0fe29
Ageu Moraes <ageumoraes67@gmail.com>
2024-04-23 13:55:45 -0300

    Melhorando a acessibilidade do código

--- AlgoritimoFatorialEmPython/calculoFatorial.py
+++ AlgoritimoFatorialEmPython/calculoFatorial.py
@@ -1,46 +1,46 @@
 
-class CalculaFatorial:
+class FactorialCalculate:
 
     def __init__(self):
         print("========================")
         print("====CALCULA FATORIAL====")
         print("========================")
-        self.fatorial = 0 # Vai receber o número dado pelo usuário
-        self.resultado = 1 # Variável contadora que armazenará o resultado
+        self.factorial = 0 # Will receive a number given by the user
+        self.result = 1 # Counter that will store the result
     
-    def get_valor(self): # Captura o valor dado pelo usuário
+    def get_value(self): # To get the value given by the user
 
-        self.fatorial = int(input("Digite um valor para saber o fatorial do mesmo: "))
-        self.resultado = 1
+        self.factorial = int(input("Digite um valor para saber o fatorial do mesmo: "))
+        self.result = 1
 
-    def verifica_condicoes(self): # Verifica a condição necessária para ser considerado fatorial
-        if self.fatorial < 0: # Se um valor negativo for inserido, vai aparecer essa mensagem
+    def checks_conditions(self): # Checks the necessary conditions to have a factorial
+        if self.factorial < 0: # if a negative value is entered it will show this message
             print("Valor negativo, portanto resultado inválido.")
-        else: # Se não, o código chama a implementação da lógica e chama o resultado
-            # Dentro de um range entre 1 ao valor inserido +1 ele será múltiplicado com a variável "Resultado"
-            # EXEMPLO: 5! = 1*1 = 1*2 = 2*3 = 6*4 = 24 * 5 = 120
-            for i in range(1, self.fatorial +1):
-                self.resultado *= i
-            self.imprime_resultado() 
+        else: # Otherwise, the code calls the logic implementation and calls the result
+            # In a range of 1 to value entered +1 it will multiply with the "Result" variable 
+            # EXEMPLE: 5! = 1*1 = 1*2 = 2*3 = 6*4 = 24 * 5 = 120
+            for i in range(1, self.factorial +1):
+                self.result *= i
+            self.print_result() 
             
 
-    def imprime_resultado(self): # Entrega o resultado
-        print(f"O fatorial do seu número é: {self.resultado}")
+    def print_result(self): # Delivers results
+        print(f"O fatorial do seu número é: {self.result}")
 
-calculadora = CalculaFatorial() # Objeto 1
-calculadora.get_valor() # // Capturando valor do usuário
-calculadora.verifica_condicoes() # Verificando se o valor inserido é válido
+calculator = FactorialCalculate() # Object 1
+calculator.get_value() # // to Get user value
+calculator.checks_conditions() # Checks if the entered value is valid
 
 
-#Forma simplificada de calcular fatorial
-#def calcula_fatorial(numero):
-#    fatorial = 1
-#    if numero >= 0:
-#        for i in range(1, numero+1):
-#            fatorial *= i
-#        print(f"O fatorial é: {fatorial}")
+# Simple form of calculating factorial
+#def factorial_calculater(number):
+#    factorial = 1
+#    if number >= 0:
+#        for i in range(1, number+1):
+#            factorial *= i
+#        print(f"O fatorial é: {factorial}")
 #    else:
 #        print("Valor inválido")
 
-#n = int(input("Digite um número: "))
-#calcula_fatorial(n)
+#number = int(input("Digite um número: "))
+#factorial_calculater(number)
